563c2bf19b2607ff6a71cab5e669966ecadec5a4
Carlos <carlos.e.santaniello@gmail.com>
2020-01-16 16:07:11 +0100

    Add construct method

--- jobs.php
+++ jobs.php
@@ -3,33 +3,49 @@
 class Job {
   private $title;
   public $description;
-  public $visible;
+  public $visible = true;
   public $months;
 
+  public function __construct($title, $description){
+    $this->setTitle($title);
+    $this->description = $description;
+  }
+
   public function setTitle($title){
+    if($title == ''){
+      $this->title = 'N/A';
+    } else {
     $this->title = $title;
+    }
   }
 
   public function getTitle() {
     return $this->title;
   }
+
+  public function getDurationAsString() {
+    $years = floor($this->months / 12);
+    $extraMonths = $this->months % 12;
+  
+    return "$years years $extraMonths months";
+  }
 }
 
-$job1 = new Job();
-$job1 -> setTitle('PHP Developer');
-$job1 -> description = 'This is an awesome job!!!';
-$job1 -> visible = true;   
+$job1 = new Job('PHP Developer', 'This is an awesome job!!!');   
 $job1 -> months = 16; 
 
 $job2 = new Job();
-$job2 -> setTitle('Python Developer');
-$job2 -> description = 'This is an awesome job!!!';
-$job2 -> visible = true;   
+$job2 -> setTitle('Python Developer', 'This is an awesome job!!!');  
 $job2 -> months = 24;
+
+$job3 = new Job();
+$job3 -> setTitle('Devops', 'This is an awesome job!!!');  
+$job3 -> months = 32;
 
 $jobs = [
   $job1,
-  $job2
+  $job2,
+  $job3
 /*  [
     'title' => ,
     '' => ,
@@ -58,22 +74,15 @@ $jobs = [
   ]*/
 ];
 
-function getDuration($months) {
-  $years = floor($months / 12);
-  $extraMonths = $months % 12;
-
-  return "$years years $extraMonths months";
-}
-
 function printJob($job) {
   if($job -> visible == false) {
     return;
   }
 
   echo '<li class="work-position">';
-  echo '<h5>' . $job -> getTitle . '</h5>';
-  echo '<p>' . $job -> description . '</p>';
-  echo '<p>' . getDuration($job -> months) . '</p>';
+  echo '<h5>' . $job->getTitle . '</h5>';
+  echo '<p>' . $job->description . '</p>';
+  echo '<p>' . $job->getDurationAsString() . '</p>';
   echo '<strong>Achievements:</strong>';
   echo '<ul>';
   echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
