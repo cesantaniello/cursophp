29c1f4d44a267dd7dcb9eaa91022c27b15b1aed6
Carlos <carlos.e.santaniello@gmail.com>
2020-01-16 15:10:36 +0100

    Add class job

--- jobs.php
+++ jobs.php
@@ -1,11 +1,40 @@
 <?php
 
+class Job {
+  private $title;
+  public $description;
+  public $visible;
+  public $months;
+
+  public function setTitle($title){
+    $this->title = $title;
+  }
+
+  public function getTitle() {
+    return $this->title;
+  }
+}
+
+$job1 = new Job();
+$job1 -> setTitle('PHP Developer');
+$job1 -> description = 'This is an awesome job!!!';
+$job1 -> visible = true;   
+$job1 -> months = 16; 
+
+$job2 = new Job();
+$job2 -> setTitle('Python Developer');
+$job2 -> description = 'This is an awesome job!!!';
+$job2 -> visible = true;   
+$job2 -> months = 24;
+
 $jobs = [
-  [
-    'title' => 'PHP Developer',
-    'description' => 'This is an awesome job!!!',
-    'visible' => true,
-    'months' => 16
+  $job1,
+  $job2
+/*  [
+    'title' => ,
+    '' => ,
+    'visible' => ,
+    'months' => 
   ],
   [
     'title' => 'Python Dev',
@@ -26,7 +55,7 @@ $jobs = [
     'title' => 'Frontend Dev',
     'visible' => true,
     'months' => 3
-  ]
+  ]*/
 ];
 
 function getDuration($months) {
@@ -37,14 +66,14 @@ function getDuration($months) {
 }
 
 function printJob($job) {
-  if($job['visible'] == false) {
+  if($job -> visible == false) {
     return;
   }
 
   echo '<li class="work-position">';
-  echo '<h5>' . $job['title'] . '</h5>';
-  echo '<p>' . $job['description'] . '</p>';
-  echo '<p>' . getDuration($job['months']) . '</p>';
+  echo '<h5>' . $job -> getTitle . '</h5>';
+  echo '<p>' . $job -> description . '</p>';
+  echo '<p>' . getDuration($job -> months) . '</p>';
   echo '<strong>Achievements:</strong>';
   echo '<ul>';
   echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
